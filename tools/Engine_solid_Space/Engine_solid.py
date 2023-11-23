5640c11b2a3c58dbb74d42a1113f9e892689107a
raul7gs <116161286+raul7gs@users.noreply.github.com>
2023-11-23 16:52:55 +0100

    Add files via upload

--- tools/Engine_solid_Space/Engine_solid.py
+++ tools/Engine_solid_Space/Engine_solid.py
@@ -1,54 +1,65 @@
 import xml.etree.ElementTree as ET
 
 
-tree = ET.parse('ToolInput/toolInput.xml')
-root = tree.getroot()
+def read_input(path):
 
-thrust = 0
-mdot = 0
+    tree = ET.parse(path)
+    root = tree.getroot()
 
-mdot_dic = {"SRB": 5290,
-            "P80": 764,
-            "GEM60": 463.18}
+    srb = len(root.findall("Stage/Engines/Solid/SRB"))
+    p80 = len(root.findall("Stage/Engines/Solid/P80"))
+    gem60 = len(root.findall("Stage/Engines/Solid/GEM60"))
 
-try:
-    SRB = len(root.findall("Stage/Engines/Solid/SRB"))
-except:
-    SRB = 0
-
-try:
-    P80 = len(root.findall("Stage/Engines/Solid/P80"))
-except:
-    P80 = 0
-
-try:
-    GEM60 = len(root.findall("Stage/Engines/Solid/GEM60"))
-except:
-    GEM60 = 0
-
-thrust_per_engine = {"SRB": 12.45e6,
-                     "P80": 2.1e6,
-                     "GEM60": 1.245e6}
-
-if SRB + P80 + GEM60 > 0:
-    thrust = SRB * thrust_per_engine["SRB"] + P80 * thrust_per_engine["P80"] + GEM60 * thrust_per_engine["GEM60"]
-if SRB > 0:
-    mdot = mdot_dic["SRB"] * SRB
-if P80 > 0:
-    mdot = mdot_dic["P80"] * P80
-if GEM60 > 0:
-    mdot = mdot_dic["GEM60"] * GEM60
+    return srb, p80, gem60
 
 
-root_output_tree = ET.Element('Rocket')
-stage_tree = ET.SubElement(root_output_tree, 'Stage')
-engines_tree = ET.SubElement(stage_tree, 'Engines')
-thrust_tree = ET.SubElement(engines_tree, 'Thrust')
-mdot_tree = ET.SubElement(engines_tree, 'mdot')
+def calculate(srb, p80, gem60):
 
-thrust_tree.text = str(thrust)
-mdot_tree.text = str(mdot)
+    thrust = 0
+    mdot = 0
 
-tree_output = ET.ElementTree(root_output_tree)
-tree_output.write('ToolOutput/toolOutput.xml')
+    mdot_dic = {"SRB": 5290,
+                "P80": 764,
+                "GEM60": 463.18}
 
+    thrust_per_engine = {"SRB": 12.45e6,
+                         "P80": 2.1e6,
+                         "GEM60": 1.245e6}
+
+    if srb + p80 + gem60 > 0:
+        thrust = srb * thrust_per_engine["SRB"] + p80 * thrust_per_engine["P80"] + gem60 * thrust_per_engine["GEM60"]
+    if srb > 0:
+        mdot = mdot_dic["SRB"] * srb
+    if p80 > 0:
+        mdot = mdot_dic["P80"] * p80
+    if gem60 > 0:
+        mdot = mdot_dic["GEM60"] * gem60
+
+    return thrust, mdot
+
+
+def write_output(path, thrust, mdot):
+
+    root_output_tree = ET.Element('Rocket')
+    stage_tree = ET.SubElement(root_output_tree, 'Stage')
+    engines_tree = ET.SubElement(stage_tree, 'Engines')
+    thrust_tree = ET.SubElement(engines_tree, 'Thrust')
+    mdot_tree = ET.SubElement(engines_tree, 'mdot')
+
+    thrust_tree.text = str(thrust)
+    mdot_tree.text = str(mdot)
+
+    tree_output = ET.ElementTree(root_output_tree)
+    tree_output.write(path)
+    pass
+
+
+def run():
+
+    srb, p80, gem60 = read_input('ToolInput/toolinput.xml')
+    thrust, mdot = calculate(srb, p80, gem60)
+    write_output('ToolOutput/toolOutput.xml', thrust, mdot)
+
+
+if __name__ == '__main__':
+    run()
