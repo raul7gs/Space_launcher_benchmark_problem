7edc7b6ff4af1d78472592df152dce9a24a34c70
raul7gs <116161286+raul7gs@users.noreply.github.com>
2023-11-27 16:31:48 +0100

    Add files via upload

--- tools/Structural_constraint/Structural_constraint.py
+++ tools/Structural_constraint/Structural_constraint.py
@@ -1,22 +1,38 @@
+"""Tool used for the calculation of the vehicle structural constraint. It is checked if the maximum loads
+during the trajectory phase overpass at any moment the maximum load the structure can withstand."""
+
 import xml.etree.ElementTree as ET
 import ast
 import numpy
 from ambiance import Atmosphere
 
-def modified_atmosphere(x):
-    if x < 80000 and x > 0:
+
+def modified_atmosphere(x):    # Atmospheric model
+
+    if 80000 > x > 0:
         rho = Atmosphere(x).density
     else:
         rho = Atmosphere(80000).density
     return rho
 
-tree = ET.parse('ToolInput/toolInput.xml')
-root = tree.getroot()
 
-qmax = float(root.find("Structure/Max_q").text)
+def read_input(path):
+    """Inputs from the XML file are read."""
 
-try:
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    qmax = float(root.find("Structure/Max_q").text)    # Maximum dynamic pressure in the whole mission
     h = ast.literal_eval(root.find("Trajectory/Height").text)
+    v = numpy.array(ast.literal_eval(root.find("Trajectory/Velocity").text))
+
+    return qmax, h, v
+
+
+def calculate(qmax, h, v):
+    """Calculation of the launcher structural constraint. It is considered to have overpassed the maximum
+    structural load if it is positive."""
+
     rho = []
     for altitude in h:
         if altitude < 0:
@@ -24,23 +40,36 @@ try:
         else:
             rho.append(modified_atmosphere(altitude).tolist()[0])
 
-
-    v = numpy.array(ast.literal_eval(root.find("Trajectory/Velocity").text))
     rho_array = numpy.array(rho)
     qvector = 0.5 * numpy.multiply(numpy.multiply(v, v), rho_array)
-
-    difference = numpy.subtract(qvector,qmax * numpy.ones(len(qvector)))
-
+    difference = numpy.subtract(qvector, qmax * numpy.ones(len(qvector)))
     constraint = numpy.max(difference)
-except:
-    constraint = 10000
 
-root_output_tree = ET.Element('Rocket')
-structure_tree = ET.SubElement(root_output_tree, 'Structure')
-constraint_tree = ET.SubElement(structure_tree, 'Constraint')
+    return constraint
 
-constraint_tree.text = str(constraint)
 
-tree_output = ET.ElementTree(root_output_tree)
-tree_output.write('ToolOutput/toolOutput.xml')
+def write_output(path, constraint):
+    """Generation of the output XML file"""
 
+    root_output_tree = ET.Element('Rocket')
+    structure_tree = ET.SubElement(root_output_tree, 'Structure')
+    constraint_tree = ET.SubElement(structure_tree, 'Constraint')
+
+    constraint_tree.text = str(constraint)
+
+    tree_output = ET.ElementTree(root_output_tree)
+    tree_output.write(path)
+
+    pass
+
+
+def run():
+    """Execution of the tool"""
+
+    qmax, h, v = read_input('ToolInput/toolinput.xml')
+    constraint = calculate(qmax, h, v)
+    write_output('ToolOutput/toolOutput.xml', constraint)
+
+
+if __name__ == '__main__':
+    run()
