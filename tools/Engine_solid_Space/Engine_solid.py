1a66d768d554e5f409baecede447782b1ff1e170
raul7gs <116161286+raul7gs@users.noreply.github.com>
2023-11-27 15:46:27 +0100

    Add files via upload

--- tools/Engine_solid_Space/Engine_solid.py
+++ tools/Engine_solid_Space/Engine_solid.py
@@ -1,11 +1,19 @@
+"""Tool used for the calculation regarding propulsion when a solid propellant is used. The input is an
+XML file containing the engines used in a certain stage. It provides as output the total maximum thrust the
+ stage can provide, as well as its mass flow.
+
+ More information about the references used can be found in the following link:"""
+
 import xml.etree.ElementTree as ET
 
 
 def read_input(path):
+    """Inputs from the XML file are read."""
 
     tree = ET.parse(path)
     root = tree.getroot()
 
+    # The number of engines of each possible stage are taken from the XML input file.
     srb = len(root.findall("Stage/Engines/Solid/SRB"))
     p80 = len(root.findall("Stage/Engines/Solid/P80"))
     gem60 = len(root.findall("Stage/Engines/Solid/GEM60"))
@@ -14,15 +22,16 @@ def read_input(path):
 
 
 def calculate(srb, p80, gem60):
+    """Calculation of the stage propulsion properties."""
 
     thrust = 0
     mdot = 0
 
-    mdot_dic = {"SRB": 5290,
+    mdot_dic = {"SRB": 5290,    # Mass flow in kg/s
                 "P80": 764,
                 "GEM60": 463.18}
 
-    thrust_per_engine = {"SRB": 12.45e6,
+    thrust_per_engine = {"SRB": 12.45e6,    # Thrust in N
                          "P80": 2.1e6,
                          "GEM60": 1.245e6}
 
@@ -39,6 +48,7 @@ def calculate(srb, p80, gem60):
 
 
 def write_output(path, thrust, mdot):
+    """Generation of the output XML file"""
 
     root_output_tree = ET.Element('Rocket')
     stage_tree = ET.SubElement(root_output_tree, 'Stage')
@@ -51,10 +61,12 @@ def write_output(path, thrust, mdot):
 
     tree_output = ET.ElementTree(root_output_tree)
     tree_output.write(path)
+
     pass
 
 
 def run():
+    """Execution of the tool"""
 
     srb, p80, gem60 = read_input('ToolInput/toolinput.xml')
     thrust, mdot = calculate(srb, p80, gem60)
